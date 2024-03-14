d8cbce8f00741b2bcb4353982bb14cad89673990
linkscope <linkscope@foxmail.com>
2024-03-15 06:13:00 +0800

    chore: checker插件配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint src/**/*.{ts,tsx}',
+        useFlatConfig: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
+})
