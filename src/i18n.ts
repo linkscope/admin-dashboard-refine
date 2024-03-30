2e1b552f4f39c20887d5dd7bf5e22b6ac067eace
linkscope <linkscope@foxmail.com>
2024-03-31 04:44:58 +0800

    fix: 修复国际化部署失效问题

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,20 @@
 import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next' // https://react.i18next.com/latest/using-with-hooks
-import Backend from 'i18next-http-backend' // For lazy loading for translations: https://github.com/i18next/i18next-http-backend
-import detector from 'i18next-browser-languagedetector' // For auto detecting the user language: https://github.com/i18next/i18next-browser-languageDetector
+import { initReactI18next } from 'react-i18next'
+import cn from './locales/cn.json'
 
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 dayjs.locale('zh-cn')
 
-i18n
-  .use(Backend)
-  .use(detector)
-  .use(initReactI18next)
-  .init({
-    supportedLngs: ['cn'],
-    backend: {
-      loadPath: '/locales/{{lng}}.json', // locale files path
+i18n.use(initReactI18next).init({
+  lng: 'cn',
+  resources: {
+    cn: {
+      translation: cn,
     },
-    fallbackLng: ['cn'],
-  })
+  },
+  supportedLngs: ['cn'],
+  fallbackLng: ['cn'],
+})
 
 export default i18n
